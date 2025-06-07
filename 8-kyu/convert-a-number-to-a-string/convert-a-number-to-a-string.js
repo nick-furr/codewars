922bb365b49694ab000dba865d3fb83a9a1cf3ff
nick-furr <nick.furr35@gmail.com>
2025-06-06 20:57:18 -0700

    Add solution - CodeHub

---
+++ 8-kyu/convert-a-number-to-a-string/convert-a-number-to-a-string.js
@@ -0,0 +1,15 @@
+/*
+function numberToString(num) {
+  // Return a string of the number here!
+}
+*/
+​
+function numberToString(num) {
+  return num.toString();
+}
+​
+​
+function numberToString(num) {
+  return String(num)
+}
+​
