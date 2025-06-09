e3a86119257a662abffb5dee7742d4c36cc2b02d
nick-furr <nick.furr35@gmail.com>
2025-06-08 21:51:23 -0700

    Add solution - CodeHub

---
+++ 8-kyu/return-negative/return-negative.js
@@ -0,0 +1,11 @@
+function makeNegative(num) {
+  if(num > 0){
+    return num * -1
+  }else if(num <= 0){
+    return num
+  }
+}
+​
+// return positive num inputs as negative; return num for 0 and negative inputs
+​
+​
