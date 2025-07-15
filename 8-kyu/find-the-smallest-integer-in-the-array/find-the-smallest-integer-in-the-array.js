9fe9a0f0a84327fd737747cbc70280b615a560e2
nick-furr <nick.furr35@gmail.com>
2025-07-14 19:41:44 -0700

    Add solution - CodeHub

---
+++ 8-kyu/find-the-smallest-integer-in-the-array/find-the-smallest-integer-in-the-array.js
@@ -0,0 +1,9 @@
+function findSmallestInt(args) {
+  let smallest = args[0];
+  for (let i = 1; i < args.length; i++) {
+    if (args[i] < smallest) {
+      smallest = args[i];
+    }
+  }
+  return smallest;
+}
