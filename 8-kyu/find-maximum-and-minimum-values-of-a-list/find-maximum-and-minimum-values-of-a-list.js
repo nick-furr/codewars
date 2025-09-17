c5b7784972af7053d822f7c15286875304c8eca8
nick-furr <nick.furr35@gmail.com>
2025-09-17 16:38:46 -0700

    Add solution - CodeHub

---
+++ 8-kyu/find-maximum-and-minimum-values-of-a-list/find-maximum-and-minimum-values-of-a-list.js
@@ -0,0 +1,9 @@
+function min(list) {
+  // Using Math.min with the spread operator to find the minimum value in the array.
+  return Math.min(...list);
+}
+​
+function max(list) {
+  // Using Math.max with the spread operator to find the maximum value in the array.
+  return Math.max(...list);
+}
