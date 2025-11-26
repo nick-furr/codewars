eb5168e5b84b937b6c5a631237ac6e7fc15a6d98
nick-furr <nick.furr35@gmail.com>
2025-11-25 23:40:08 -0800

    Add solution - CodeHub

---
+++ 8-kyu/total-amount-of-points/total-amount-of-points.js
@@ -0,0 +1,12 @@
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+​
+// function points(games) {
+//   return games.reduce((output,current)=>{
+//     let x = parseInt(current[0]);
+//     let y = parseInt(current[2]);
+//     let value= x>y ? 3 : x===y ? 1 : 0;
+//     return output+value;
+//   },0)
+// }
